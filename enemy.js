ea678222590b39178bc31b057d9249701d6d91b5
unknown <tombirbeck47@gmail.com>
2023-04-10 15:16:39 +0100

    enemies move and bounce off sides

--- enemy.js
+++ enemy.js
@@ -1,14 +1,19 @@
 export default class Enemy{
-    constructor(x, y, color, health) {
+    constructor(x, y, color, health, directionX, directionY) {
         this.x = x;
         this.y = y;
         this.color = color;
         this.health = health;
         this.width = 50;
         this.height = 50;
+        this.speed = 2;
+        this.directionX = directionX;
+        this.directionY = directionY;
     }
 
     draw(ctx) {
+        this.moveX(this.directionX)
+        this.moveY(this.directionY);
         ctx.fillStyle = this.color;
         if(this.health > 1) {
             ctx.strokeStyle = 'white';
@@ -24,7 +29,52 @@ export default class Enemy{
         ctx.fillText(this.health, this.x + this.width/5, this.y + this.height/1.5)
     }
 
+    moveX(direction){
+        const moveLeft = () => {
+            this.x -= this.speed;
+        }
+        const moveRight = () => {
+            this.x += this.speed;
+        }
+        
+        if (direction === 'left'){
+            moveLeft()
+        }
+        else if (direction === 'right'){
+            moveRight()
+        }
+    }
+    moveY(direction){
+        const moveUp = () => {
+            this.y -= this.speed;
+        }
+        const moveDown = () => {
+            this.y += this.speed;
+        }
+        
+        if (direction === 'down'){
+            moveDown()
+        }
+        else if (direction === 'up'){
+            moveUp()
+        }
+    }
+
     takeDamage(damage) {
         this.health -= damage;
+    }
+
+    isEnemyAtLeftEdge(enemy){
+        return enemy.x <= 0;
+    }
+    isEnemyAtRightEdge(enemy){
+        return enemy.x + this.width >= 550;
+    }
+
+    isEnemyAtTopEdge(enemy){
+        return enemy.y <= 0;
+    }
+    isEnemyAtBottomEdge(enemy){
+        return enemy.y + this.height >= 500;
     }
 }
--- main.js
+++ main.js
@@ -22,16 +22,16 @@ const bulletController = new BulletController(canvas);
 const player = new Player(canvas.width/2.2, canvas.height/1.3, damage, bulletController);
 
 let enemies = [
-    new Enemy(50, 20, randomEnemyColor(), getRandomInt(1,20)),
-    new Enemy(150, 20, randomEnemyColor(), getRandomInt(1,20)),
-    new Enemy(250, 20, randomEnemyColor(), getRandomInt(1,20)),
-    new Enemy(350, 20, randomEnemyColor(), getRandomInt(1,20)),
-    new Enemy(450, 20, randomEnemyColor(), getRandomInt(1,20)),
-    new Enemy(50, 100, randomEnemyColor(), getRandomInt(1,20)),
-    new Enemy(150, 100, randomEnemyColor(), getRandomInt(1,20)),
-    new Enemy(250, 100, randomEnemyColor(), getRandomInt(1,20)),
-    new Enemy(350, 100, randomEnemyColor(), getRandomInt(1,20)),
-    new Enemy(450, 100, randomEnemyColor(), getRandomInt(1,20)),
+    new Enemy(50, 20, randomEnemyColor(), getRandomInt(1,20), 'left', 'down'),
+    new Enemy(150, 20, randomEnemyColor(), getRandomInt(1,20), 'left', 'down'),
+    new Enemy(250, 20, randomEnemyColor(), getRandomInt(1,20), 'left', 'down'),
+    new Enemy(350, 20, randomEnemyColor(), getRandomInt(1,20), 'left', 'down'),
+    new Enemy(450, 20, randomEnemyColor(), getRandomInt(1,20), 'left', 'down'),
+    new Enemy(50, 100, randomEnemyColor(), getRandomInt(1,20), 'left', 'down'),
+    new Enemy(150, 100, randomEnemyColor(), getRandomInt(1,20), 'left', 'down'),
+    new Enemy(250, 100, randomEnemyColor(), getRandomInt(1,20), 'left', 'down'),
+    new Enemy(350, 100, randomEnemyColor(), getRandomInt(1,20), 'left', 'down'),
+    new Enemy(450, 100, randomEnemyColor(), getRandomInt(1,20), 'left', 'down'),
 ]
 
 const gameLoop = () => {
@@ -42,6 +42,17 @@ const gameLoop = () => {
     player.draw(ctx);
     timer.textContent = time;
     enemies.forEach((enemy) => {
+        if (enemy.isEnemyAtLeftEdge(enemy)){
+            enemy.directionX = 'right'
+        } else if (enemy.isEnemyAtRightEdge(enemy)){
+            enemy.directionX = 'left'
+        }
+        if (enemy.isEnemyAtBottomEdge(enemy)){
+            enemy.directionY = 'up'
+        }
+        else if (enemy.isEnemyAtTopEdge(enemy)){
+            enemy.directionY = 'down'
+        }
         if(bulletController.collideWith(enemy)){
             if(enemy.health <= 0){
                 const index = enemies.indexOf(enemy);
@@ -56,16 +67,16 @@ const gameLoop = () => {
         })
     if (enemies.length <= 0){
         enemies = [
-            new Enemy(50, 20, randomEnemyColor(), getRandomInt(1,20)),
-            new Enemy(150, 20, randomEnemyColor(), getRandomInt(1,20)),
-            new Enemy(250, 20, randomEnemyColor(), getRandomInt(1,20)),
-            new Enemy(350, 20, randomEnemyColor(), getRandomInt(1,20)),
-            new Enemy(450, 20, randomEnemyColor(), getRandomInt(1,20)),
-            new Enemy(50, 100, randomEnemyColor(), getRandomInt(1,20)),
-            new Enemy(150, 100, randomEnemyColor(), getRandomInt(1,20)),
-            new Enemy(250, 100, randomEnemyColor(), getRandomInt(1,20)),
-            new Enemy(350, 100, randomEnemyColor(), getRandomInt(1,20)),
-            new Enemy(450, 100, randomEnemyColor(), getRandomInt(1,20)),
+            new Enemy(50, 20, randomEnemyColor(), getRandomInt(1,20), 'left'),
+            new Enemy(150, 20, randomEnemyColor(), getRandomInt(1,20), 'left'),
+            new Enemy(250, 20, randomEnemyColor(), getRandomInt(1,20), 'left'),
+            new Enemy(350, 20, randomEnemyColor(), getRandomInt(1,20), 'left'),
+            new Enemy(450, 20, randomEnemyColor(), getRandomInt(1,20), 'left'),
+            new Enemy(50, 100, randomEnemyColor(), getRandomInt(1,20), 'left'),
+            new Enemy(150, 100, randomEnemyColor(), getRandomInt(1,20), 'left'),
+            new Enemy(250, 100, randomEnemyColor(), getRandomInt(1,20), 'left'),
+            new Enemy(350, 100, randomEnemyColor(), getRandomInt(1,20), 'left'),
+            new Enemy(450, 100, randomEnemyColor(), getRandomInt(1,20), 'left'),
         ]
     }
 }
@@ -93,16 +104,16 @@ const res = () => {
     score = 0;
     time = 20;
     enemies = [
-        new Enemy(50, 20, randomEnemyColor(), getRandomInt(1,20)),
-        new Enemy(150, 20, randomEnemyColor(), getRandomInt(1,20)),
-        new Enemy(250, 20, randomEnemyColor(), getRandomInt(1,20)),
-        new Enemy(350, 20, randomEnemyColor(), getRandomInt(1,20)),
-        new Enemy(450, 20, randomEnemyColor(), getRandomInt(1,20)),
-        new Enemy(50, 100, randomEnemyColor(), getRandomInt(1,20)),
-        new Enemy(150, 100, randomEnemyColor(), getRandomInt(1,20)),
-        new Enemy(250, 100, randomEnemyColor(), getRandomInt(1,20)),
-        new Enemy(350, 100, randomEnemyColor(), getRandomInt(1,20)),
-        new Enemy(450, 100, randomEnemyColor(), getRandomInt(1,20)),
+        new Enemy(50, 20, randomEnemyColor(), getRandomInt(1,20), 'left'),
+        new Enemy(150, 20, randomEnemyColor(), getRandomInt(1,20), 'left'),
+        new Enemy(250, 20, randomEnemyColor(), getRandomInt(1,20), 'left'),
+        new Enemy(350, 20, randomEnemyColor(), getRandomInt(1,20), 'left'),
+        new Enemy(450, 20, randomEnemyColor(), getRandomInt(1,20), 'left'),
+        new Enemy(50, 100, randomEnemyColor(), getRandomInt(1,20), 'left'),
+        new Enemy(150, 100, randomEnemyColor(), getRandomInt(1,20), 'left'),
+        new Enemy(250, 100, randomEnemyColor(), getRandomInt(1,20), 'left'),
+        new Enemy(350, 100, randomEnemyColor(), getRandomInt(1,20), 'left'),
+        new Enemy(450, 100, randomEnemyColor(), getRandomInt(1,20), 'left'),
     ]
 }
 
